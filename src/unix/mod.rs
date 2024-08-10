aa5af0eadb186f1b773b9172b5277e278b7a565b
Sergei Solomatov <solomatovs@gmail.com>
2024-08-10 05:43:23 +0000

    temp work

--- src/unix/mod.rs
+++ src/unix/mod.rs
@@ -1,5 +1,3 @@
-mod unix;
-
 use std::io::Write;
 use std::os::unix::io::{AsFd, AsRawFd, FromRawFd};
 use std::os::unix::process::CommandExt;
@@ -29,7 +27,7 @@ use clap::{Arg, ArgGroup, ArgMatches, Command};
 pub enum CliError {
     StdIoError(std::io::Error),
 
-    NixErrorno(nix::errno::Errno),
+    NixErrorno(),
 
     ArgumentError(String),
 
@@ -50,7 +48,7 @@ impl From<std::io::Error> for CliError {
 
 impl From<nix::errno::Errno> for CliError {
     fn from(error: nix::errno::Errno) -> Self {
-        CliError::NixErrorno(error)
+        CliError::NixErrorno()
     }
 }
 
@@ -123,166 +121,6 @@ pub fn set_termsize(fd: i32, mut size: Box<nix::libc::winsize>) -> std::io::Resu
     match ret {
         0 => Ok(()),
         _ => Err(std::io::Error::last_os_error()),
-    }
-}
-
-fn cli() -> Command {
-    let cmd = Command::new("sshpass")
-        .version("1.0")
-        .about("Non-interactive ssh password provider")
-        .arg(
-            Arg::new("password")
-                .short('p')
-                .long("password")
-                .value_name("PASSWORD")
-                .help("Provide password as argument (security unwise)"),
-        )
-        .arg(
-            Arg::new("filename")
-                .short('f')
-                .long("file")
-                .value_name("FILENAME")
-                .help("Take password to use from file"),
-        )
-        .arg(
-            Arg::new("fd")
-                .short('d')
-                .long("fd")
-                .value_name("FD")
-                .help("Use number as file descriptor for getting password"),
-        )
-        .arg(
-            Arg::new("env")
-                .short('e')
-                .long("env")
-                .value_name("ENV")
-                .help("Password is passed as env-var 'SSHPASS'"),
-        )
-        .arg(
-            Arg::new("prompt")
-                .short('P')
-                .long("prompt")
-                .value_name("PROMPT")
-                .help("Which string should sshpass search for to detect a password prompt"),
-        )
-        .arg(
-            Arg::new("verbose")
-                .short('v')
-                .long("verbose")
-                .value_name("VERBOSE")
-                .help("Be verbose about what you're doing"),
-        )
-        .arg(
-            Arg::new("otp-secret")
-                .long("otp-secret")
-                .help("One time secret in argument"),
-        )
-        .arg(
-            Arg::new("otp-secret-file")
-                .long("otp-secret-file")
-                .help("One time secret in file"),
-        )
-        .arg(
-            Arg::new("otp-secret-env")
-                .env("SSHPASS_OTP_SECRET")
-                .long("otp-secret-env")
-                .help("One time secret is passed as env"),
-        )
-        .arg(
-            Arg::new("otp-secret-fd")
-                .long("otp-secret-fd")
-                .help("Use number as file descriptor for getting otp secret"),
-        )
-        .arg(
-            Arg::new("otp-code")
-                .long("otp-code")
-                .help("One time code in argument"),
-        )
-        .arg(
-            Arg::new("otp-code-file")
-                .long("otp-code-file")
-                .help("One time code in file"),
-        )
-        .arg(
-            Arg::new("otp-code-env")
-                .env("SSHPASS_OTP_CODE")
-                .long("otp-code-env")
-                .help("One time code is passed as env"),
-        )
-        .arg(
-            Arg::new("otp-code-fd")
-                .long("otp-code-fd")
-                .help("Use number as file descriptor for getting otp code"),
-        )
-        .arg(
-            Arg::new("otp-prompt")
-                .short('O')
-                .long("otp-prompt")
-                .help("Which string should sshpass search for the one time password prompt"),
-        )
-        .group(
-            ArgGroup::new("password-conflict")
-                .args(["password"])
-                .conflicts_with_all(["filename", "fd", "env"]),
-        )
-        .group(
-            ArgGroup::new("otp-conflict")
-                .args(["otp-secret"])
-                .conflicts_with_all([
-                    "otp-secret-file",
-                    "otp-secret-fd",
-                    "otp-secret-env",
-                    "otp-code",
-                    "otp-code-file",
-                    "otp-code-fd",
-                    "otp-code-env",
-                ]),
-        )
-        .arg(
-            Arg::new("program")
-                .help("Program to execute")
-                .required(true)
-                .num_args(1),
-        )
-        .arg(
-            Arg::new("program_args")
-                .help("arguments that will be passed to the program being run")
-                .required(false)
-                .num_args(1..)
-                .allow_hyphen_values(true)
-                .trailing_var_arg(true),
-        );
-
-    cmd
-}
-
-fn main() {
-    if let Ok(level) = std::env::var("SSHPASS_LOG") {
-        let level = log::LevelFilter::from_str(&level).unwrap();
-
-        let config = simplelog::ConfigBuilder::new()
-            .set_time_format_rfc3339()
-            .set_time_offset_to_local()
-            .unwrap()
-            .set_max_level(level)
-            .build();
-
-        let _ = simplelog::CombinedLogger::init(vec![simplelog::WriteLogger::new(
-            level,
-            config,
-            std::fs::File::create("sshpass.log").unwrap(),
-        )])
-        .unwrap();
-    }
-
-    let args = cli().get_matches();
-    trace!("mach arguments {:#?}", args);
-
-    trace!("args ok, start program");
-    let status = run(args);
-
-    if let Err(e) = status {
-        error!("exit with error: {:?}", e);
     }
 }
 
@@ -405,7 +243,7 @@ fn run(args: ArgMatches) -> Result<(), CliError> {
                     }
                     Err(nix::errno::Errno::EINTR) => {
                         error!("waitpid error: {}", nix::errno::Errno::EINTR);
-                        break Err(CliError::NixErrorno(nix::errno::Errno::EINTR));
+                        break Err(CliError::NixErrorno());
                     }
                     Err(e) => {
                         error!("waitpid error: {}", e);
@@ -605,68 +443,9 @@ fn run(args: ArgMatches) -> Result<(), CliError> {
                 std::time::SystemTime::now(),
                 e
             );
-            Err(CliError::NixErrorno(e))
+            Err(CliError::NixErrorno())
         }
     };
 
     status
 }
-
-fn _strip_nl(s: &mut String) -> String {
-    if s.ends_with('\n') {
-        s.pop();
-        if s.ends_with('\r') {
-            s.pop();
-        }
-    }
-    let out: String = s.to_string();
-    out
-}
-
-// // Функция для чтения пароля в зависимости от аргументов командной строки
-// fn _get_password(matches: &clap::ArgMatches) -> String {
-//     if let Some(&fd) = matches.get_one::<i32>("fd") {
-//         // Дублируем файловый дескриптор и читаем пароль
-//         let fd_dup = dup(fd).expect("Failed to duplicate file descriptor");
-//         let mut fd_file = unsafe { File::from_raw_fd(fd_dup) };
-//         let mut password = String::new();
-//         fd_file
-//             .read_to_string(&mut password)
-//             .expect("Failed to read password from file descriptor");
-//         drop(fd_file); // Закрываем файл, так как он нам больше не нужен
-//         password
-//     } else if let Some(password) = env::var("SSHPASS").ok() {
-//         // Использование переменной окружения SSHPASS
-//         password
-//     } else {
-//         // Ввод пароля с клавиатуры
-//         println!("Enter Password:");
-//         let mut pass = TermRead::read_passwd(&mut std::io::stdin(), &mut std::io::stdout())
-//             .expect("Failed to read password from tty")
-//             .expect("Failed to read password from tty");
-//         let pass = _strip_nl(&mut pass);
-//         pass
-//         // rpassword::read_password().expect("Failed to read password from tty")
-//     }
-// }
-
-// fn _get_totp(_matches: &clap::ArgMatches) -> String {
-//     let secret = _matches
-//         .get_one::<String>("totp_secret")
-//         .expect("totp secret is required");
-//     _generate_totp(secret)
-//     // "get_totp".into()
-// }
-
-// fn _generate_totp(secret: &str) -> String {
-//     let totp = TOTP::new(
-//         Algorithm::SHA1,
-//         6,
-//         1,
-//         30,
-//         Secret::Raw(secret.as_bytes().to_vec()).to_bytes().unwrap(),
-//     )
-//     .unwrap();
-//     let token = totp.generate_current().unwrap();
-//     token
-// }
